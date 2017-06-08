49ae672cb353983b2771d44e33ee2866ba9de76b
BaezCrdrm <dev.baez_crdrm@outlook.com>
2017-06-08 16:18:59 -0500

    Agregadas nuevas actividades

---
+++ app/src/main/java/mx/unam/primera/com/appmoviles/NoConnectionActivity.java
@@ -0,0 +1,15 @@
+package mx.unam.primera.com.appmoviles;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NoConnectionActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_no_connection);
+    }
+}
